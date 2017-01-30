86e014bc3808f0100b82ca99f725b01970b3b9f8
viggy815 <viggy815@aol.com>
2017-01-30 18:14:49 -0500

    design
    This is the basic design. I attempted to get all the layouts complete
    but I am still working on them I want everyone to have access to what I
    have so far so I am committing the changes. I currently left off on the
    last layout where we will display the clinic data with the map fragment.
    Everything is not fully functional but this is a starting point.

---
+++ app/src/main/java/com/example/teamb/teambhealthcareapplication/Clinic_Search.java
@@ -0,0 +1,19 @@
+package com.example.teamb.teambhealthcareapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Clinic_Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinic__search);
+    }
+    public void searchListview(View view){
+        //code for searching the database for a clinic
+        //display results in the current listview
+        //upon clicking pulls up fragment with clinic data
+    }
+}
