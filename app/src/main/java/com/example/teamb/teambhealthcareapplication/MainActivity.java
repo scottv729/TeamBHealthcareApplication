a9b511e58517025fdcd95111993560b5e48e14d1
viggy815 <viggy815@aol.com>
2017-01-31 20:41:43 -0500

    Revert "design"
    This reverts commit 86e014bc3808f0100b82ca99f725b01970b3b9f8.

--- app/src/main/java/com/example/teamb/teambhealthcareapplication/MainActivity.java
+++ app/src/main/java/com/example/teamb/teambhealthcareapplication/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.teamb.teambhealthcareapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void startSearching(View view) {
-        //starts the clinic search activity
-        Intent intent = new Intent(MainActivity.this, Clinic_Search.class);
-        startActivity(intent);
     }
 }
