a327968a7555ae1cfae1c5d07ec798398df7d0a4
viggy815 <viggy815@aol.com>
2017-02-01 02:25:35 -0500

    added class
    commit my user interface branch and then this branch. please. Thanks.
    this file didnt go with that other commit

---
+++ app/src/main/java/com/example/teamb/teambhealthcareapplication/ClinicSearch.java
@@ -0,0 +1,21 @@
+
+package com.example.teamb.teambhealthcareapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ClinicSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinic_search);
+    }
+
+    public void searchListview(View view) {
+        //code for searching the database for a clinic
+        //display results in the current listview
+        //upon clicking pulls up fragment with clinic data
+    }
+}
